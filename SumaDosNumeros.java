e1d6a55767ec0bfae38aa57d0598bc0df55059dd
ProfEduardo67FullStack <profeduardo67.cap@gmail.com>
2025-10-28 13:48:26 -0600

    Probando el entorno con algoritmos básicos

---
+++ SumaDosNumeros.java
@@ -0,0 +1,9 @@
+public class SumaDosNumeros{
+    public static void main(String[] args) {
+        int n1,n2,suma;
+        n1=2;
+        n2=3;
+        suma=n1+n2;
+        System.out.println("La suma de los números es: " + suma);
+    }
+}
